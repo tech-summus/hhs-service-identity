e4cbd81971ca80fbbf9ab7a119c3c45260dedf38
Hasan H. SAHIN <hasansahin@araskargo.com.tr>
2024-07-05 22:02:55 +0300

    ok

--- src/Hhs.IdentityService.EntityFrameworkCore/Migrations/App/IdentityAppDbContextModelSnapshot.cs
+++ src/Hhs.IdentityService.EntityFrameworkCore/Migrations/App/IdentityAppDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace Hhs.IdentityService.EntityFrameworkCore.Migrations.App
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.18")
+                .HasAnnotation("ProductVersion", "8.0.6")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
